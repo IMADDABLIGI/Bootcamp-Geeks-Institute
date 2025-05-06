3d7aeafbeb6ca82a23d55a7cf3a44f2973e1ec04
IMADDABLIGI <imad.dabligi@gmail.com>
2025-05-06 12:11:32 +0100

    refactor(master): Fix exercise2 issue

--- Week1/Day2/Exercises/exercise2.py
+++ Week1/Day2/Exercises/exercise2.py
@@ -2,35 +2,35 @@
 
 # family["test"] = 23
 
-def calculate(age):
-    if age < 3:
-        return 0
-    elif age >= 3 and age <= 12:
-        return 10
-    else:
-        return 15
+family = {}
 
+def calculate(family_dic):
+    sum = 0
+    for name, age in family_dic.items():
+        if age < 3:
+            continue
+        elif age >= 3 and age <= 12:
+            sum += 10
+        else:
+            sum += 15
+    print(f"Your total cost for the movies is {sum}$")
 
-sum = 0
-check = True
-
-while check:
+while True:
     name = input("name (Type quit if u finished): ").lower()
     if name == "quit":
-        check = False
         break
     age = input("age: ")
     if not age.isdigit() or not name.isalpha():
-        print("Please provide a valide name and age")
+        print("<-----> Error: Please provide a valide name and age! <----->")
         continue
     else:
-        sum += calculate(int(age))
+        family[name] = int(age)
 
-print(f"Your total cost for the movies is {sum}$")
+calculate(family)
     
 
-# sum = 0
 
+# sum = 0
 # for age in family.values():
 #     if age < 3:
 #         continue
