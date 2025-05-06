fc10e37e8e452a0c0d2103a26a3271462a11eeec
IMADDABLIGI <imad.dabligi@gmail.com>
2025-05-06 14:51:55 +0100

    feat(master): Finished exercise6

---
+++ Week1/Day2/Exercises/exercise6.py
@@ -0,0 +1,9 @@
+def make_shirt(size="Large", text="I love Python"):
+    print(f"The size of the shirt is <{size}> and the text is <{text}>")
+
+make_shirt("Medium", "C is better than Python, change my mind hh")
+make_shirt("Large")
+make_shirt("Medium")
+make_shirt("s", "Lmra t9ra walakin 7adha sads hh")
+make_shirt(size="XL", text="Java is the father of JS, change my mind")
+
