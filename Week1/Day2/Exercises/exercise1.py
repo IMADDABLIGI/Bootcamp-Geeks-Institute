4ea1f54080539f18876dc47c65755c28d2e21705
IMADDABLIGI <imad.dabligi@gmail.com>
2025-05-07 18:08:16 +0100

    feat(master): Finished challenge Gold

--- Week1/Day2/Exercises/exercise1.py
+++ Week1/Day2/Exercises/exercise1.py
@@ -1,14 +1,15 @@
 keys = ['Ten', 'Twenty', 'Thirty']
+keys1 = ['Ten', 'Twenty', 'Thirty']
 values = [10, 20, 30]
 
 
 # my_dict = zip(keys, values)
-my_dict = dict(zip(keys, values))
+# my_dict = dict(zip(keys, keys1, values))
 
-# for key,value in dict(zip(keys, values)).items():
-#     print(key, '', value)
+for key,value, value2 in zip(keys, keys1, values):
+    print(key, '', value)
 
 # for item in zip(keys, values):
 #     print(item)
 
-print(my_dict)
+# print(my_dict)
