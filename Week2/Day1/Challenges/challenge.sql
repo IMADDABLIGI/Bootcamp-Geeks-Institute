a57ecbad93a9d2a8bf872c2d2db8b41e33717591
IMADDABLIGI <imad.dabligi@gmail.com>
2025-05-12 17:48:26 +0100

    feat(master): Finished challenge Day1

---
+++ Week2/Day1/Challenges/challenge.sql
@@ -0,0 +1,9 @@
+SELECT COUNT(*) FROM actors;
+
+-- Try to add a new actor with some blank fields. What do you think the outcome will be ?
+-- The output of this SQL Syntax will be an error :
+
+-- INSERT INTO actors(first_name, last_name, age)
+-- VALUES (, ,);
+
+-- Because I specified when creating the table to not have a NULL.
