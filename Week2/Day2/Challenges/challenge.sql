a1cab195472993194b139fef6414a5989d6994e3
IMADDABLIGI <imad.dabligi@gmail.com>
2025-05-13 19:29:21 +0100

    refactor(master): Challenge Finished

---
+++ Week2/Day2/Challenges/challenge.sql
@@ -0,0 +1,9 @@
+-- Q1
+
+-- The output will be 0 because there is no id == NULL
+
+--Q2 output is 2 the values (6,'Sharlee'), (7,'Krish')
+
+-- Q3: 0, because there is no id = 5 or NULL
+
+-- Q4: output will be 2 which are the count of the values (6,'Sharlee'), (7,'Krish') Again :/
