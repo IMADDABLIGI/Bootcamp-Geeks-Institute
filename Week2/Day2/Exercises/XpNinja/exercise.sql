8e12b1ffe7aa5f39912ac948a8ff1fe0e3b85a1c
IMADDABLIGI <imad.dabligi@gmail.com>
2025-05-14 09:11:22 +0100

    refactor(master): Finished exerciseNinja

---
+++ Week2/Day2/Exercises/XpNinja/exercise.sql
@@ -0,0 +1,31 @@
+SELECT *
+FROM customer
+ORDER BY first_name ASC
+OFFSET (
+  SELECT COUNT(*) FROM customer
+) - 2 LIMIT 2;
+
+DELETE FROM payment
+WHERE customer_id = (
+  SELECT customer_id FROM customer WHERE first_name = 'Scott'
+);
+
+SELECT * FROM customer WHERE first_name = 'Scott';
+
+SELECT 
+    p.payment_id,
+    c.first_name,
+    c.last_name,
+    p.amount,
+    p.payment_date
+FROM payment p
+LEFT JOIN customer c ON p.customer_id = c.customer_id;
+
+SELECT 
+    p.payment_id,
+    c.first_name,
+    c.last_name,
+    p.amount,
+    p.payment_date
+FROM payment p
+INNER JOIN customer c ON p.customer_id = c.customer_id;
