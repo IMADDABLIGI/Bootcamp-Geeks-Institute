02625b03fc52c634c0b08925b7c134727ecb8cc9
IMADDABLIGI <imad.dabligi@gmail.com>
2025-05-15 14:37:22 +0100

    refactor(master): Finished exerciseXp

--- Week2/Day4/Exercises/Xp/menu_manager.py
+++ Week2/Day4/Exercises/Xp/menu_manager.py
@@ -61,6 +61,12 @@ class MenuManager():
     
 
 if __name__ == "__main__":
+    # item = MenuItem('Burger', 35)
+    # item.save()
+    # item.delete()
+    # item.update('Veggie Burger', 37)
+    item2 = MenuManager.get_by_name('Beef Stew')
+    items = MenuManager.all()
     item2 = MenuManager.get_by_name('Veggie Burgy')
     print(item2)
     print(MenuManager.all_items())
