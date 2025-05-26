b72aa7ded7cb5d0efd0cc6f52e962bffc3cde817
IMADDABLIGI <imad.dabligi@gmail.com>
2025-05-26 09:41:32 +0100

    refactor(master): Exercise1

---
+++ Week4/Day1/Exercises/Xp/exercise1.js
@@ -0,0 +1,22 @@
+// 1 We will get an error bcs U cant reassign a const variable
+
+// 2 We will get an error bcs U cant reassign a const variable AGAIN :/
+
+// 
+
+function funcFour() {
+    window.a = "hello";
+}
+
+
+function funcFive() {
+    alert(`inside the funcFive function ${a}`);
+}
+
+// #3.1 - run in the console:
+funcFour()
+funcFive()
+
+// 4 it will work because the vaiable is being declared both in global and local scope
+
+// 5 it will work because the vaiable is being declared both in global and local scope AGAIN :/
