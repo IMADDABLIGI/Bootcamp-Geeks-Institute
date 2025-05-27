47878bff04cf1235f0e06e500c625af847185015
IMADDABLIGI <imad.dabligi@gmail.com>
2025-05-27 09:40:37 +0100

    refactor(master): Tricky one

---
+++ Week4/Day2/Exercises/XpNinja/exercise3.js
@@ -0,0 +1,19 @@
+const users = [{ firstName: 'Bradley', lastName: 'Bouley', role: 'Full Stack Resident' },
+             { firstName: 'Chloe', lastName: 'Alnaji', role: 'Full Stack Resident' },
+             { firstName: 'Jonathan', lastName: 'Baughn', role: 'Enterprise Instructor' },
+             { firstName: 'Michael', lastName: 'Herman', role: 'Lead Instructor' },
+             { firstName: 'Robert', lastName: 'Hajek', role: 'Full Stack Resident' },
+             { firstName: 'Wes', lastName: 'Reid', role: 'Instructor'},
+             { firstName: 'Zach', lastName: 'Klabunde', role: 'Instructor'}];
+
+
+const usersRole = users.map((user, key) => {
+    const fullName = user.firstName + user.lastName
+
+    let obj = {
+        [fullName] : user.role
+    }
+    return obj;
+})
+
+console.log(usersRole)
