9e8a8cf1a24b7d3c85c1e7c315c0ec2af6c864b8
IMADDABLIGI <imad.dabligi@gmail.com>
2025-05-28 09:21:10 +0100

    refactor(master): Flamingo

---
+++ Week4/Day3/Exercises/XpNinja/exercise.js
@@ -0,0 +1,16 @@
+class Bird {
+  constructor() {
+    console.log("I'm a bird. 🦢");
+  }
+}
+
+class Flamingo extends Bird {
+  constructor() {
+    console.log("I'm pink. 🌸");
+    super();
+  }
+}
+
+const pet = new Flamingo();
+
+// The first conole.log will be the child class than the parent class like every oop language
