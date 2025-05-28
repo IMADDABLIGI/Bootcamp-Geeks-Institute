0faedd97cd605c5151151e2b1a7649fcca41bfa3
IMADDABLIGI <imad.dabligi@gmail.com>
2025-05-28 08:59:11 +0100

    refactor(master): Challenge finished

--- Week4/Day3/Challenges/script.js
+++ Week4/Day3/Challenges/script.js
@@ -4,4 +4,15 @@ class Video{
         this.uploader = uploader;
         this.time = time; // time in seconds
     }
+
+    watch(){
+        console.log(`uploader ${this.uploader} watched all time ${this.time} of title ${this.title}!`)
+    }
+
 }
+
+let mp4 = new Video("mp4", "uploader1", 120);
+mp4.watch();
+
+let openheimer = new Video("Oppenheimer", "uploader2", 180);
+openheimer.watch();
