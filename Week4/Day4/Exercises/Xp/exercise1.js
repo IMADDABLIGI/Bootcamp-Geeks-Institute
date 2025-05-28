9ec621e2420bac12e8c350453502512d36e5fa95
IMADDABLIGI <imad.dabligi@gmail.com>
2025-05-28 13:08:21 +0100

    refactor(master): Finished exercisesXp

--- Week4/Day4/Exercises/Xp/exercise1.js
+++
@@ -1,3 +0,0 @@
-// Hi There :/
-
-// The data will be appeared in the url as arguments :/
--- Week4/Day4/Exercises/Xp/exercise2.js
+++
@@ -1,2 +0,0 @@
-// Hi Again :/
-// Tha Data will be sent to the path provided in the action attribute which is the formData.html
