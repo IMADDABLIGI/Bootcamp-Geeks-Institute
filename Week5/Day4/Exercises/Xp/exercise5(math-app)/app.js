c879b7f94bf12a3e9b54524cd24223727831266b
IMADDABLIGI <imad.dabligi@gmail.com>
2025-06-06 09:11:01 +0100

    refactor(master): Math Calculation

--- Week5/Day4/Exercises/Xp/exercise5(math-app)/app.js
+++ Week5/Day4/Exercises/Xp/exercise5(math-app)/app.js
@@ -1,11 +1,11 @@
 import { add, multiply } from './math.js';
-import _ from 'lodash';
+import calculate from 'lodash';
 
 const sum = add(8, 12);
 const product = multiply(6, 7);
 
 const numbers = [100, 200, 300];
-const total = _.sum(numbers);
+const total = calculate.sum(numbers);
 
 console.log(`8 + 12 = ${sum}`);
 console.log(`6 * 7 = ${product}`);
