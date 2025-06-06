4ada051feac28175f9fecdc2a3be4a8681820f27
IMADDABLIGI <imad.dabligi@gmail.com>
2025-06-06 09:15:33 +0100

    refactor(master): 3ach Sidna

---
+++ Week5/Day4/Exercises/Xp/exercise6(npm-beginner)/app.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+
+console.log(chalk.green('Success! Everything is working'));
+console.log(chalk.blue('Allah insar sidna'));
+console.log(chalk.red.underline('Yahya Yassin'));
+console.log(chalk.yellow.bgMagenta('3ach Sidna'));
