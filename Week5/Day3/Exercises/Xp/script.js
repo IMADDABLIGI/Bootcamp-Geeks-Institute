8ed57fb73b3ae07ec68656c3c189ced3f9725a0b
IMADDABLIGI <imad.dabligi@gmail.com>
2025-06-12 10:35:51 +0100

    feat(master): Add random algorithm

--- Week5/Day3/Exercises/Xp/script.js
+++ Week5/Day3/Exercises/Xp/script.js
@@ -1,59 +1,72 @@
-// someOne = {
-//     name: '',
-//     height: '',
-//     gender: '',
-//     birth: '',
-//     homeWorld: ''
-// }
+const characterInfo = document.querySelector('div');
 
-// const fetchSomone = () => {
-//     fetch(`https://www.swapi.tech/api/people/1`)
-//     .then((response)=>{
-    //     })
-    //     .then((resData)=>{
-        //         return  response.json()
-//         console.log(resData.result.properties)
-//         return(resData.result.properties);
-//     })
-//     .catch((err)=>console.log(err.message))
-// } 
-
-
-const div = document.querySelector('div');
-
-const fetchSomone = async (randomNum) => {
+const fetchHomeworld = async (url) => {
     try {
-        const response = await fetch(`https://www.swapi.tech/api/people/${randomNum}`);
-        if (response.ok){
-            const responseData = await response.json();
-            console.log(responseData)
-            return (responseData.result.properties)
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            return data.result.properties.name;
         }
     } catch (err) {
-        console.error(err)
+        console.error('Error fetching homeworld:', err);
+        return 'Unknown';
+    }
+}
+
+const fetchCharacter = async (characterId) => {
+    try {
+        const response = await fetch(`https://www.swapi.tech/api/people/${characterId}`);
+        if (!response.ok) {
+            throw new Error('Character not found');
+        }
+        const data = await response.json();
+        const character = data.result.properties;
+        
+        const homeworldName = await fetchHomeworld(character.homeworld);
+        
+        return {
+            name: character.name,
+            height: character.height,
+            gender: character.gender,
+            birthYear: character.birth_year,
+            homeworld: homeworldName
+        };
+    } catch (err) {
+        console.error('Error fetching character:', err);
+        return null;
     }
 }
 
 const findSomeone = async () => {
-    div.innerHTML = `
+    characterInfo.innerHTML = `
         <div class="fa-3x">
             <i class="fa-solid fa-circle-notch fa-spin"></i>
         </div>
-    `
-    const randomNum = Math.floor(Math.random() * 9) + 1;
-    const someone = await fetchSomone(randomNum);
-    console.log(someone)
-    if (someone){
-        div.innerHTML = `
-        <h3> ${someone.name} </h3>
-        <h3> Height: ${someone.height} </h3>
-        <h3> Gender: ${someone.gender} </h3>
-        <h3> Birth Year: ${someone.birth_year} </h3>
-        <h3> Home World: ${someone.skin_color} </h3>
-        `
-    }
-    else
-        div.innerHTML = `<h3> No data </h3>`
+    `;
     
+    try {
+        const randomNum = Math.floor(Math.random() * 83) + 1;
+        const character = await fetchCharacter(randomNum);
+        
+        if (character) {
+            characterInfo.innerHTML = `
+                <h3>${character.name}</h3>
+                <h3>Height: ${character.height} cm</h3>
+                <h3>Gender: ${character.gender}</h3>
+                <h3>Birth Year: ${character.birthYear}</h3>
+                <h3>Home World: ${character.homeworld}</h3>
+            `;
+        } else {
+            characterInfo.innerHTML = `
+                <h3>Error loading character data</h3>
+                <i class="fas fa-exclamation-triangle"></i>
+            `;
+        }
+    } catch (err) {
+        characterInfo.innerHTML = `
+            <h3>Error loading character data</h3>
+            <i class="fas fa-exclamation-triangle"></i>
+        `;
+        console.error('Error:', err);
+    }
 }
-
